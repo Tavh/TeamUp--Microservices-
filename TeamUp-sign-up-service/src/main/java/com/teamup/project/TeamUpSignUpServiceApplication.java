84febbe843a0f3e4115a304555eb9a0f1e7fcb02
Tavh <tavherzlich26@gmail.com>
2019-03-27 22:24:29 +0200

    Init

---
+++ TeamUp-sign-up-service/src/main/java/com/teamup/project/TeamUpSignUpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamup.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamUpSignUpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamUpSignUpServiceApplication.class, args);
+	}
+
+}
