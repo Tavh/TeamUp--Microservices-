46b6ad5b240f6931aa80355bb5539b263f94e74e
Tavh <tavherzlich26@gmail.com>
2019-03-28 15:42:31 +0200

    Gateway removed

---
+++ TeamUp-gateway/src/main/java/com/teamup/project/gateway/URIConfiguration.java
@@ -0,0 +1,10 @@
+package com.teamup.project.gateway;
+
+public class URIConfiguration {
+
+    private final static String httpbin = "http://httpbin.org:80";
+
+    public String getHttpbin() {
+        return httpbin;
+    }
+}
--- TeamUp-gateway/src/test/java/com/teamup/project/TeamUpGatewayApplicationTests.java
+++ TeamUp-gateway/src/test/java/com/teamup/project/TeamUpGatewayApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class TeamUpGatewayApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 }
